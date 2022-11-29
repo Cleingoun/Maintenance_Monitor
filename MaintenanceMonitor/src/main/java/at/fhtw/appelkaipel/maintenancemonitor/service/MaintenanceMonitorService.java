2222fc639519c6b4919123d47fe56f8795e23a5f
sappel99 <simon.appel99@gmail.com>
2022-11-29 13:21:18 +0100

    Commit initial Controller, Message Class and Service #2 #4

---
+++ MaintenanceMonitor/src/main/java/at/fhtw/appelkaipel/maintenancemonitor/service/MaintenanceMonitorService.java
@@ -0,0 +1,10 @@
+package at.fhtw.appelkaipel.maintenancemonitor.service;
+
+import at.fhtw.appelkaipel.maintenancemonitor.dto.Message;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MaintenanceMonitorService {
+    private final Message message = new Message();
+}
+
