4617d71a120b851e8b2a6c3bcdf0b81d75b7f488
Kaipel Sebastian <38778737+Cleingoun@users.noreply.github.com>
2022-11-29 20:17:47 +0100

    Display a centrally stored Message (/message) #7

--- MaintenanceMonitor/src/main/java/at/fhtw/appelkaipel/maintenancemonitor/controller/MaintenanceMonitorController.java
+++ MaintenanceMonitor/src/main/java/at/fhtw/appelkaipel/maintenancemonitor/controller/MaintenanceMonitorController.java
@@ -3,7 +3,7 @@ package at.fhtw.appelkaipel.maintenancemonitor.controller;
 import at.fhtw.appelkaipel.maintenancemonitor.service.MaintenanceMonitorService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.ui.Model;
 @Controller
 public class MaintenanceMonitorController {
 
@@ -12,13 +12,18 @@ public class MaintenanceMonitorController {
     public MaintenanceMonitorController(MaintenanceMonitorService monitorService) {
         this.monitorService = monitorService;
     }
+
     @GetMapping("/home")
     public String home() {
         return "home";
     }
 
+    @GetMapping("/message")
+    String getMes(Model model) {
+        String message = monitorService.getMessage();
+        model.addAttribute("message", message);
+
+        return "mes";
+    }
+
 }
-
-
-
-
